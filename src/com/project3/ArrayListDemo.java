643cbc4f76c065389d6fecc2026a3b074b4acd18
Dakshata-22 <bhanjidakshata22@gmail.com>
2022-12-30 10:09:20 +0530

    firt array list demo program

---
+++ src/com/project3/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package com.project3;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		ArrayList list = new ArrayList();
+		list.add("dakshata");
+		list.add("bhanji");
+		list.add(100);
+		System.out.println(list);
+		
+	}
+	
+	
+}
